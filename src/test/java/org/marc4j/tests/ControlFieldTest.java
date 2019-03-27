bbd63e24feace24851d0f3b71316dba07e864e74
Luis Panadero Guardeño <luis.panadero@digibis.com>
2019-03-27 16:34:20 +0100

    Converted to jUnit4 and fixed tests

--- src/test/java/org/marc4j/tests/ControlFieldTest.java
+++ src/test/java/org/marc4j/tests/ControlFieldTest.java
@@ -1,55 +1,59 @@
 package org.marc4j.tests;
 
-import junit.framework.Assert;
-import junit.framework.TestCase;
+import static org.junit.Assert.fail;
 
-import java.text.DecimalFormat;
+import org.junit.Assert;
+import org.junit.Test;
+import org.marc4j.marc.ControlField;
+import org.marc4j.marc.IllegalDataElementException;
+import org.marc4j.marc.IllegalTagException;
 
-import org.marc4j.marc.*;
-
-public class ControlFieldTest extends TestCase {
-
-    public ControlFieldTest(String name) {
-    	super(name);
+public class ControlFieldTest
+{
+    @Test()
+    public void testConstructor()
+    {
+        char[] data = "test".toCharArray();
+        ControlField cf2 = new ControlField("003", data);
+        Assert.assertEquals(cf2.getTag(), "003");
+        Assert.assertEquals(cf2.getData(), data);
     }
 
-    public void testConstructor() {
-	char[] data = "test".toCharArray(); 
-	ControlField cf2 = new ControlField("003", data);
-	Assert.assertEquals(cf2.getTag(), "003");
-	Assert.assertEquals(cf2.getData(), data);
-    }
-
-    public void testEquals() {
-	ControlField cf1 = new ControlField("003", "test".toCharArray());
-	ControlField cf2 = new ControlField("003", "test".toCharArray());
-	ControlField cf3 = new ControlField("003", "uofiytyout".toCharArray());
+    @Test()
+    public void testEquals()
+    {
+        ControlField cf1 = new ControlField("003", "test".toCharArray());
+        ControlField cf2 = new ControlField("003", "test".toCharArray());
+        ControlField cf3 = new ControlField("003", "uofiytyout".toCharArray());
         Assert.assertEquals(cf1, cf1);
         Assert.assertEquals(cf1.hashCode(), cf1.hashCode());
-        Assert.assertTrue(!cf1.equals(cf2));
-        Assert.assertTrue(!cf1.equals(cf3));
+        Assert.assertFalse(cf1.equals(cf2)); // super.equals
+        Assert.assertFalse(cf1.equals(cf3));
 
     }
 
-    public void testSetter() {
-	ControlField cf1 = new ControlField();
-	try {
-	    cf1.setTag("010");
-	    fail("Should raise an IllegalTagException");
-	} catch (IllegalTagException success) {}
+    @Test()
+    public void testSetter()
+    {
+        ControlField cf1 = new ControlField();
+        try {
+            cf1.setTag("010");
+            fail("Should raise an IllegalTagException");
+        } catch (IllegalTagException success) {
+        }
 
-	try {
-	    cf1.setData("test");
-	    fail("Should raise an IllegalDataElementException");
-	} catch (IllegalDataElementException success) {}
+        try {
+            cf1.setData("test");
+            fail("Should raise an IllegalDataElementException");
+        } catch (IllegalDataElementException success) {
+        }
 
     }
 
-    public void testMarshal() {
-	ControlField cf1 = new ControlField("003", "test".toCharArray());
-	Assert.assertEquals(cf1.marshal(), "test");
+    @Test()
+    public void testMarshal()
+    {
+        ControlField cf1 = new ControlField("003", "test".toCharArray());
+        Assert.assertEquals(cf1.marshal(), "test");
     }
-
 }
-
-
--- src/test/java/org/marc4j/tests/TagTest.java
+++ src/test/java/org/marc4j/tests/TagTest.java
@@ -1,67 +1,70 @@
 package org.marc4j.tests;
 
-import junit.framework.Assert;
-import junit.framework.TestCase;
+import static org.junit.Assert.fail;
 
 import java.text.DecimalFormat;
 
-import org.marc4j.marc.*;
+import org.junit.Assert;
+import org.junit.Test;
+import org.marc4j.marc.IllegalTagException;
+import org.marc4j.marc.Tag;
 
-public class TagTest extends TestCase {
-
-    public TagTest(String name) {
-    	super(name);
-    }
-
+public class TagTest
+{
     private DecimalFormat df = new DecimalFormat("000");
 
-    protected void setUp() {}
-
-    public void testIsValid() {
-	for (int i = 1; i <= 999; i++) {
-	    String tag = df.format(i).toString();
-	    Assert.assertTrue(Tag.isValid(tag));
-	}
-	try {
-	    Tag.isValid("1234");
-	    fail("Should raise an IllegalTagException");
-	} catch (IllegalTagException success) {}
+    @Test()
+    public void testIsValid()
+    {
+        for (int i = 1; i <= 999; i++) {
+            String tag = df.format(i).toString();
+            Assert.assertTrue(Tag.isValid(tag));
+        }
+        try {
+            Tag.isValid("1234");
+            fail("Should raise an IllegalTagException");
+        } catch (IllegalTagException success) {
+        }
     }
 
-    public void testIsControlField() {
-	for (int i = 1; i < 10; i++) {
-	    String tag = df.format(i).toString();
-	    Assert.assertTrue(Tag.isControlField(tag));
-	}
+    @Test()
+    public void testIsControlField()
+    {
+        for (int i = 1; i < 10; i++) {
+            String tag = df.format(i).toString();
+            Assert.assertTrue(Tag.isControlField(tag));
+        }
 
-	for (int i = 10; i <= 999; i++) {
-	    String tag = df.format(i).toString();
-	    Assert.assertTrue(! Tag.isControlField(tag));
-	}
+        for (int i = 10; i <= 999; i++) {
+            String tag = df.format(i).toString();
+            Assert.assertTrue(!Tag.isControlField(tag));
+        }
     }
 
-    public void testIsControlNumberField() {
-	String tag = df.format(1).toString();
-	Assert.assertTrue(Tag.isControlNumberField(tag));
+    @Test()
+    public void testIsControlNumberField()
+    {
+        String tag = df.format(1).toString();
+        Assert.assertTrue(Tag.isControlNumberField(tag));
 
-	for (int i = 2; i <= 999; i++) {
-	    tag = df.format(i).toString();
-	    Assert.assertTrue(! Tag.isControlNumberField(tag));
-	}
+        for (int i = 2; i <= 999; i++) {
+            tag = df.format(i).toString();
+            Assert.assertTrue(!Tag.isControlNumberField(tag));
+        }
     }
 
-    public void isDataField() {
-	for (int i = 10; i <= 999; i++) {
-	    String tag = df.format(i).toString();
-	    Assert.assertTrue(Tag.isDataField(tag));
-	}
+    @Test()
+    public void isDataField()
+    {
+        for (int i = 10; i <= 999; i++) {
+            String tag = df.format(i).toString();
+            Assert.assertTrue(Tag.isDataField(tag));
+        }
 
-	for (int i = 1; i < 10; i++) {
-	    String tag = df.format(i).toString();
-	    Assert.assertTrue(! Tag.isDataField(tag));
-	}
+        for (int i = 1; i < 10; i++) {
+            String tag = df.format(i).toString();
+            Assert.assertTrue(!Tag.isDataField(tag));
+        }
     }
 
 }
-
-
